e2ca19e21a8e9fcfbe8b5d70912d6bd4932d9938
bmpickford01 <b.mccolmpickford@student.qut.edu.au>
2016-08-26 15:37:12 +1000

    Added header - link to css/js

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,20 @@
 <!DOCTYPE html>
 <?php
-echo "Home Page122123123";
+
+require '../inc/header.inc';
+
+echo "Home Page";
+
+?>
+
+<body>
+    <header id="header">
+        <div id="head">
+            <p>Stick It In</p>
+        </div>
+	</header>
+</body>
+
+<?php
+require '../inc/footer.inc';
 ?>
