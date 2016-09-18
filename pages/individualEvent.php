fb0b5217489e52159757a05c366d414b8b2fbaf8
Ionized0 <jonny.vukim@gmail.com>
2016-09-19 00:01:40 +1000

    Various changes to Event page and therefore CSS page
    Added template for sponsor information and event information (using
    tables from bootstrap)

--- pages/individualEvent.php
+++ pages/individualEvent.php
@@ -13,13 +13,36 @@
         <!-- END HOME NAVIGATION -->
 
 
-		<!-- Code for actual event details -->
-
 		<div class = "event-head">
-			<h2>City Meetup at Domino's Pizza!</h2>
+			<h1>City Meetup at Domino's Pizza!</h1>
 		</div>
-
-		<!-- Event details -->
+		<div class = "event-sponsors">
+			<h2>Sponsors</h2>
+			<h4>Coca-Cola</h4>
+			<h4>Queensland University of Technology</h4>
+		</div>
+		<div class = "row">
+			<div class = "col-md-6 event-information">
+				Details
+				<table class = "table table-bordered">
+					<tr>
+						<td>Address</td><td>105 Albert Street, BRISBANE QLD 4000</td>
+					</tr>
+					<tr>
+						<td>Date</td><td>25th September 2016 7:00pm</td>
+					</tr>
+					<tr>
+						<td>Finish</td><td>25th September 2016 9:00pm</td>
+					</tr>
+					<tr>
+						<td>Cost</td><td>To be decided...</td>
+					</tr>
+				</table>
+			</div>
+			<div class = "col-md-6 event-description">
+				meme
+			</div>
+		</div>
 
         <?php
         require '../inc/footer.inc';
