25064439e4981e2c7add99565c60b47696474154
Ionized0 <jonny.vukim@gmail.com>
2016-09-18 22:38:42 +1000

    Added code to individualEvent page and edited css to cater for it
    Added functions for event page in CSS file, added simple code to example
    individualEvent page

--- pages/individualEvent.php
+++ pages/individualEvent.php
@@ -1,3 +1,29 @@
-<?php
-echo "Indiviual event page";
-?>
+<!DOCTYPE html>
+
+<?php require '../inc/header.inc'; ?>
+
+
+
+<body>
+    <div class="container-fluid event">
+        <!-- HOME NAVIGATION -->
+        <?php
+            require '../inc/nav.inc';
+        ?>
+        <!-- END HOME NAVIGATION -->
+
+
+		<!-- Code for actual event details -->
+
+		<div class = "event-head">
+			<h2>City Meetup at Domino's Pizza!</h2>
+		</div>
+
+		<!-- Event details -->
+
+        <?php
+        require '../inc/footer.inc';
+        ?>
+    </div>
+</body>
+
