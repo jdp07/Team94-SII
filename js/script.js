57f8a896d4ef0cfc091d07ba1c29c5f2f09287e3
bmpickford01 <b.mccolmpickford@student.qut.edu.au>
2016-09-22 00:15:15 +1000

    Registration validation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
-function validateLogin(loginForm){
-	if (document.loginForm.email.value == "" || document.loginForm.password.value == ""){
+function validateLogin(loginForm) {
+	if (document.loginForm.email.value === "" || document.loginForm.password.value === "") {
 		return false;
 	}
 	return true;
 }
+
+/** Registration Validation **/
+
+/*
+function validateFName(regForm) {
+    var name = document.regForm.fName.value;
+    name = name.trim();
+    if (name === "" || !isNaN(name) || name===null) {
+        alert("Not a valid name");
+        return false;
+    }
+    return true;
+}
+
+function validateSName(regForm) {
+    var name = document.regForm.sName.value;
+    name = name.trim();
+    if (name === "" || !isNaN(name)) {
+        alert("Not a valid name");
+        return false;
+    }
+    return true;
+}
+
+function validateReg(regForm) {
+    validateFName(regForm);
+    validateSName(regForm);
+}
+*/
+
+/** END Registration Validation **/
