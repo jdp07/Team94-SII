3ff4b1ee395a656f38b9e67b30791df2920271c3
Ionized0 <jonny.vukim@gmail.com>
2016-10-04 22:51:25 +1000

    basic generic page for event list
    just made a template for event list page

--- pages/eventList.php
+++ pages/eventList.php
@@ -1,3 +1,21 @@
-<?php
-echo "List of events"
-?>
+<!DOCTYPE html>
+
+<?php require '../inc/header.inc'; ?>
+
+
+
+<body>
+    <div class="container-fluid event">
+        <!-- HOME NAVIGATION -->
+        <?php
+            require '../inc/nav.inc';
+        ?>
+        <!-- END HOME NAVIGATION -->
+
+        Event List Page
+
+        <?php
+        require '../inc/footer.inc';
+        ?>
+    </div>
+</body>
