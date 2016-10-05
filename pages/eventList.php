886f19c18c3f4bc1e806f98714d2b055e374db9c
Ionized0 <jonny.vukim@gmail.com>
2016-10-05 19:03:03 +1000

    Upcoming events boxes (static)
    Added some boxes with event information inside them
    Doesn't actually pull from the database, just hard coded information in
    them right now

--- pages/eventList.php
+++ pages/eventList.php
@@ -5,17 +5,93 @@
 
 
 <body>
-    <div class="container-fluid event">
-        <!-- HOME NAVIGATION -->
-        <?php
-            require '../inc/nav.inc';
-        ?>
-        <!-- END HOME NAVIGATION -->
+	<div class="container-fluid event">
+		<!-- Include home navigation -->
+		<?php
+			require '../inc/nav.inc';
+		?>
+		
+		<!-- Event list header -->
+		<div class = "row">
+			<div class = "col-md-4">
+				<div class = "row">
+					<div class = "col-md-2"></div>
+					<div class = "col-md-10">
+						<div class = "event-list-head">
+							<h1>Upcoming Events</h1>
+						</div>
+					</div>
+				</div>
+			</div>
 
-        Event List Page
+			<div class = "col-md-8">
+			</div>
+		</div>
+	
+		<!-- Rows of boxes -->
+		<div class = "row">
 
-        <?php
-        require '../inc/footer.inc';
-        ?>
-    </div>
+			<!-- Col 1-->
+			<div class = "col-md-4">
+				<div class = "row">
+					<div class = "col-md-2"></div>
+					<div class = "col-md-8 event-list-info">
+						<div class = "event-list-info-name">Event name</div>
+						<div class = "event-list-info-heading">Date: </div>
+						<div id = "event-list-info-date">Tuesday 1st November 2016</div>
+						<div class = "event-list-info-heading">Time: </div>
+						<div id = "event-list-info-time">1:00pm</div>
+						<div class = "event-list-info-heading">Location: </div>
+						<div id = "event-list-info-location">80 Albert Street, Brisbane 4000</div>
+						<br/>
+						<center><button type="button" class="btn btn-event">More info</button></center>
+					</div>
+					<div class = "col-md-2"></div>
+				</div>
+			</div>
+			
+			<!-- Col 2 -->
+			<div class = "col-md-4">
+				<div class = "row">
+					<div class = "col-md-2"></div>
+					<div class = "col-md-8 event-list-info">
+						<div class = "event-list-info-name">Event name</div>
+						<div class = "event-list-info-heading">Date: </div>
+						<div id = "event-list-info-date">Tuesday 1st November 2016</div>
+						<div class = "event-list-info-heading">Time: </div>
+						<div id = "event-list-info-time">1:00pm</div>
+						<div class = "event-list-info-heading">Location: </div>
+						<div id = "event-list-info-location">80 Albert Street, Brisbane 4000</div>
+						<br/>
+						<center><button type="button" class="btn btn-event">More info</button></center>
+					</div>
+					<div class = "col-md-2"></div>
+				</div>
+			</div>
+
+			<!-- Col 3 -->
+			<div class = "col-md-4">
+				<div class = "row">
+					<div class = "col-md-2"></div>
+					<div class = "col-md-8 event-list-info">
+						<div class = "event-list-info-name">Event name</div>
+						<div class = "event-list-info-heading">Date: </div>
+						<div id = "event-list-info-date">Tuesday 1st November 2016</div>
+						<div class = "event-list-info-heading">Time: </div>
+						<div id = "event-list-info-time">1:00pm</div>
+						<div class = "event-list-info-heading">Location: </div>
+						<div id = "event-list-info-location">80 Albert Street, Brisbane 4000</div>
+						<br/>
+						<center><button type="button" class="btn btn-event">More info</button></center>
+					</div>
+					<div class = "col-md-2"></div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Include footer -->
+		<?php
+			require '../inc/footer.inc';
+		?>
+	</div>
 </body>
