4a3c9090ff4319132378fef70531537b69fbad88
Ionized0 <jonny.vukim@gmail.com>
2016-10-10 15:17:36 +1000

    Created functions for eventList.php, all information dynamic besides button
    button still needs work posting correct information to
    individualEvent.php

--- pages/eventList.php
+++ pages/eventList.php
@@ -2,7 +2,6 @@
 
 <?php 
 	require '../inc/header.inc';
-	require '../inc/setPDO.inc';
 ?>
 
 
@@ -16,13 +15,51 @@
 
 			//creatHeading creates appropriate headings
 			function createHeading($heading) {
+
+				if($heading == "Upcoming Events") {
+					require '../inc/setPDO.inc';
+					//Find number of upcoming events
+					try {
+						$statement = $pdo->prepare('SELECT COUNT(*) AS numUpcomingEvents FROM EVENTS_TB WHERE eventDate > CURDATE()');
+						$statement->execute();
+					}
+					catch (PDOException $e) {
+						echo $e->getMessage();
+					}
+
+					foreach($statement as $result) {
+						$numEvents = $result['numUpcomingEvents'];
+					}
+				}
+
+				else {
+					require '../inc/setPDO.inc';
+					//Find number of past events
+					try {
+						$statement = $pdo->prepare('SELECT COUNT(*) AS numPastEvents FROM EVENTS_TB WHERE eventDate < CURDATE()');
+						$statement->execute();
+					}
+					catch (PDOException $e) {
+						echo $e->getMessage();
+					}
+
+					foreach($statement as $result) {
+						$numEvents = $result['numPastEvents'];
+					}
+				}
+
 				echo('<div class = "row">');
 				echo('	<div class = "col-md-4">');
 				echo('		<div class = "row">');
 				echo('			<div class = "col-md-2"></div>');
 				echo('			<div class = "col-md-10">');
-				echo('				<div class = "event-list-head"');
-				echo('					<h1>'.$heading.'</h1>');
+				echo('				<div class = "event-list-head">');
+				if ($heading == "Upcoming Events") {
+					echo('				<h1>'.$heading.'<br/><small> '.$numEvents.' coming soon...</small></h1>');
+				}
+				else {
+					echo('				<h1>'.$heading.'<br/><small> '.$numEvents.' events finished!</small></h1>');
+				}
 				echo('				</div>');
 				echo('			</div>');
 				echo('		</div>');
@@ -33,23 +70,153 @@
 
 			//createBoxes creates event information boxes for the number of events under the given heading
 			function createBoxes($type) {
+				require '../inc/setPDO.inc';
 				if($type == "upcoming") {
 					//Find number of upcoming events
-					
+					try {
+						$statement = $pdo->prepare('SELECT COUNT(*) AS numUpcomingEvents FROM EVENTS_TB WHERE eventDate > CURDATE()');
+						$statement->execute();
+					}
+					catch (PDOException $e) {
+						echo $e->getMessage();
+					}
+
+					foreach($statement as $result) {
+						$numUpcomingEvents = $result['numUpcomingEvents'];
+					}
+
+					//Find information about upcoming events
+					try {
+						$statement = $pdo->prepare('SELECT * FROM EVENTS_TB where eventDate > CURDATE()');
+						$statement->execute();
+					}
+					catch (PDOException $e) {
+						echo $e->getMessage();
+					}
+
+					//Counter to know when to make rows
+					$boxNum = 1;
+
+					//Fetch results from db
+					foreach($statement as $result) {
+						$id = $result['eventID'];
+						$name = $result['eventName'];
+						$when = $result['eventDate'];
+						$address = $result['eventAddress'];
+
+						$date = new DateTime($when);
+						$date = $date->format('d M Y h:ia');
+
+						if(($boxNum + 2) % 3 == 0) {
+							echo('<div class = "row">');
+						}
+
+						echo('<div class = "col-md-4">');
+						echo('	<div class = "row">');
+						echo('		<div class = "col-md-2"></div>');
+						echo('		<div class = "col-md-8 event-list-info"><br/>');
+						echo('			<div class = "event-list-info-name">'.$name.'</div>');
+						echo('			<div class = "event-list-info-heading">Date: </div>');
+						echo('			<div id = "event-list-info-date">'.$date.'</div>');
+						echo('			<div class = "event-list-info-heading">Location: </div>');
+						echo('			<div id = "event-list-info-location">'.$address.'</div><br/>');
+						echo('			<center><form action = "individualEvent.php?eventID='.$id.'"><input type="submit" class="btn btn-event" value = "More info" name = "eventID"></form></center><br/>');
+						echo('		</div>');
+						echo('	<div class = "col-md-2"></div>');
+						echo('	</div>');
+						echo('</div>');
+
+						if(($boxNum % 3 == 0) || ($boxNum == $numUpcomingEvents)) {
+							echo('</div>');
+						}
+
+						$boxNum++;
+					}
 				}
 
 				else if($type == "past") {
+					//Find number of past events
+					try {
+						$statement = $pdo->prepare('SELECT COUNT(*) AS numPastEvents FROM EVENTS_TB WHERE eventDate < CURDATE()');
+						$statement->execute();
+					}
+					catch (PDOException $e) {
+						echo $e->getMessage();
+					}
 
+					foreach($statement as $result) {
+						$numPastEvents = $result['numPastEvents'];
+					}
+
+					//Find information about upcoming events
+					try {
+						$statement = $pdo->prepare('SELECT * FROM EVENTS_TB where eventDate < CURDATE()');
+						$statement->execute();
+					}
+					catch (PDOException $e) {
+						echo $e->getMessage();
+					}
+
+					//Counter to know when to make rows
+					$boxNum = 1;
+
+					//Fetch results from db
+					foreach($statement as $result) {
+						$id = $result['eventID'];
+						$name = $result['eventName'];
+						$when = $result['eventDate'];
+						$address = $result['eventAddress'];
+
+						$date = new DateTime($when);
+						$date = $date->format('d M Y h:ia');
+
+						if(($boxNum + 2) % 3 == 0) {
+							echo('<div class = "row">');
+						}
+
+						echo('<div class = "col-md-4">');
+						echo('	<div class = "row">');
+						echo('		<div class = "col-md-2"></div>');
+						echo('		<div class = "col-md-8 event-list-info"><br/>');
+						echo('			<div class = "event-list-info-name">'.$name.'</div>');
+						echo('			<div class = "event-list-info-heading">Date: </div>');
+						echo('			<div id = "event-list-info-date">'.$date.'</div>');
+						echo('			<div class = "event-list-info-heading">Location: </div>');
+						echo('			<div id = "event-list-info-location">'.$address.'</div><br/>');
+						echo('			<center><form action = "individualEvent.php?eventID='.$id.'"><input type="submit" class="btn btn-event" value = "More info"></form></center><br/>');
+						echo('		</div>');
+						echo('	<div class = "col-md-2"></div>');
+						echo('	</div>');
+						echo('</div>');
+
+						if(($boxNum % 3 == 0) || ($boxNum == $numPastEvents)) {
+							echo('</div>');
+						}
+
+						$boxNum++;
+					}
 				}
 
 				else {
 					//Nothing should happen
 				}
 			}
+
+			//Upcoming Event list header
+			createHeading('Upcoming Events');
+
+			//Upcoming Event boxes
+			createBoxes('upcoming');
+
+			//Past Event list header
+			createHeading('Past Events');
+
+			//Past Event boxes
+			createBoxes('past');
 		?>
 		
 		<!-- Event list header -->
-		<div class = "row">
+		<!-- <div class = "row">
 			<div class = "col-md-4">
 				<div class = "row">
 					<div class = "col-md-2"></div>
@@ -61,12 +228,10 @@
 				</div>
 			</div>
 			<div class = "col-md-8"></div>
-		</div>
+		</div> -->
 	
 		<!-- Rows of boxes -->
-		<div class = "row">
-
-			<!-- Col 1-->
+<!-- 		<div class = "row">
 			<div class = "col-md-4">
 				<div class = "row">
 					<div class = "col-md-2"></div>
@@ -87,7 +252,6 @@
 				</div>
 			</div>
 			
-			<!-- Col 2 -->
 			<div class = "col-md-4">
 				<div class = "row">
 					<div class = "col-md-2"></div>
@@ -108,7 +272,6 @@
 				</div>
 			</div>
 
-			<!-- Col 3 -->
 			<div class = "col-md-4">
 				<div class = "row">
 					<div class = "col-md-2"></div>
@@ -128,10 +291,10 @@
 					<div class = "col-md-2"></div>
 				</div>
 			</div>
-		</div>
+		</div> -->
 
 		<!-- Past event list header -->
-		<div class = "row">
+		<!-- <div class = "row">
 			<div class = "col-md-4">
 				<div class = "row">
 					<div class = "col-md-2"></div>
@@ -146,11 +309,9 @@
 			<div class = "col-md-8">
 			</div>
 		</div>
-
+ -->
 		<!-- Rows of boxes -->
-		<div class = "row">
-
-			<!-- Col 1-->
+<!-- 		<div class = "row">
 			<div class = "col-md-4">
 				<div class = "row">
 					<div class = "col-md-2"></div>
@@ -171,7 +332,6 @@
 				</div>
 			</div>
 			
-			<!-- Col 2 -->
 			<div class = "col-md-4">
 				<div class = "row">
 					<div class = "col-md-2"></div>
@@ -190,7 +350,7 @@
 					</div>
 					<div class = "col-md-2"></div>
 				</div>
-			</div>
+			</div> -->
 
 		<!-- Include footer -->
 		<?php
